b32be9742baaa7e5a9fb2e43b39d5d08311d1a5c
Henry Koiti Arasaki <henry_arasaki@hotmail.com>
2023-02-03 00:42:07 -0300

    fetch all tasks

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,20 +2,35 @@ import { useState, useEffect } from "react";
 import { api } from "../service/api";
 import Header from "../compoents/Header";
 
+
+type Task = {
+    id: number;
+    title: string;
+    done: boolean;
+    createdAt: string
+    lastUpdatedAt:string
+  };
+
+type GetTasksResponse = {
+        tasks:Task[]
+  };
+
 export default function Home(){
-    const [tasks,setTasks] = useState([])
+    const [tasks,setTasks] = useState<Task[]>([])
 
     useEffect(()=>{
-        api.get()
+        api.get<GetTasksResponse>("/api/tasks")
+        .then(response=>setTasks(response.data.tasks))
+        // .then((res)=>console.log(res.data.tasks))
+        .then(()=>console.log(tasks))
     },[])
     
 
     return(<>
     <Header/>
     <div>
+        {tasks.map((task,index)=><h4 key={index}>{task.title}</h4>)}
 
     </div>
-    
-    
     </>)
 }
